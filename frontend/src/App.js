ecb7b0fdf6eaedf1ededb3aedf8cf57e8988ecec
Kevin Rosenberg <kevinr20@tlu.ee>
2022-11-11 14:03:48 +0200

    basic project parts
    folders in BE and FE, page templates in code, removed few unnecessary files and small code changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [show, setShow] = useState(true)
+
   return (
-    <div className="App">
+    <Box className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* 
+        siia tuleb routes 
+        */}
       </header>
-    </div>
+    </Box>
   );
 }
 
