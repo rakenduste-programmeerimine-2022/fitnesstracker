dbc210ef91bf4e352e47400353ea669e58bff9ea
Kevin Rosenberg <kevinr20@tlu.ee>
2022-11-13 14:57:58 +0200

    Css changes for home page title
    Added extra styles for home page title

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,25 +6,29 @@ const Home = () => {
 
     return (
         <Box>
-            <Typography align="center" variant="h2">Fitness Tracker</Typography>
-            <Button variant="contained" href="" disabled>
-                Log Out
-            </Button>
+            <Box className="title-box">
+                <Typography className="title-text" align="center" variant="h2">Fitness Tracker</Typography>
+            </Box>
+            <Box className="button-bar">
+                <Button className="logout" variant="contained" href="" disabled>
+                    Log Out
+                </Button>
+            </Box>
 
-            <Box className="menu_container">
+            <Box className="menu-container">
                 <Box className="menu">
-                    <Button className="menu_button button1" variant="contained" href="http://localhost:3000/bodytrack">
+                    <Button className="menu-button" variant="contained" href="http://localhost:3000/bodytrack">
                         Track Body
                     </Button>
-                    <Button className="menu_button button2" variant="contained" href="http://localhost:3000/calorietrack">
+                    <Button className="menu-button" variant="contained" href="http://localhost:3000/calorietrack">
                         Track Calories
                     </Button>
                 </Box>
                 <Box className="menu">
-                    <Button className="menu_button button3" variant="contained" href="http://localhost:3000/statsgraph">
+                    <Button className="menu-button" variant="contained" href="http://localhost:3000/statsgraph">
                         Visualize Changes
                     </Button>
-                    <Button className="menu_button button4" variant="contained" href="http://localhost:3000/comparison">
+                    <Button className="menu-button" variant="contained" href="http://localhost:3000/comparison">
                         Change Comparison
                     </Button>
                 </Box>
