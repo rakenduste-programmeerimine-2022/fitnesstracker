fde8b4cbb19a229801befbf63647fd4068e941a1
Kevin Rosenberg <kevinr20@tlu.ee>
2022-11-22 21:02:10 +0200

    Old body form replaced with newer

--- frontend/src/pages/BodyTracking.js
+++ frontend/src/pages/BodyTracking.js
@@ -5,21 +5,6 @@ import BodyForm from "../components/BodyForm"
 
 const BodyTracking = () => {
 
-    const [inputs, setInputs] = useState({});
-
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}))
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(inputs);
-        
-
-    }
-
     return (
         <Box>
             <Box sx={{ flexGrow: 1 }} className="mui-page-title">
@@ -51,89 +36,6 @@ const BodyTracking = () => {
                 <Box>
                     <Button className="picture-button" variant="outlined">Uploadable Picture</Button>
                 </Box>
-                {/* <form className="body-form" onSubmit={handleSubmit}>
-                    
-                    <TextField
-                        className="body-field"
-                        name="weight"
-                        value={inputs.weight || ""}
-                        onChange={handleChange} 
-                        label="Weight"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        className="body-field"
-                        name="bicep"
-                        value={inputs.bicep || ""}
-                        onChange={handleChange} 
-                        label="Bicep Circumference"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        className="body-field"
-                        name="forearm"
-                        value={inputs.forearm || ""}
-                        onChange={handleChange}
-                        label="Forearm Circumference"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        className="body-field"
-                        name="torso"
-                        value={inputs.torso || ""} 
-                        onChange={handleChange}
-                        label="Torso Circumference"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        className="body-field"
-                        name="waist"
-                        value={inputs.waist || ""}
-                        onChange={handleChange}
-                        label="Waist Circumference"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        className="body-field"
-                        name="quad"
-                        value={inputs.quad || ""}
-                        onChange={handleChange}
-                        label="Quad Circumference"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        className="body-field"
-                        name="calve"
-                        value={inputs.calve || ""}
-                        onChange={handleChange}
-                        label="Calve Circumference"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <Box className="save-button">
-                        <Button variant="contained" type="submit">Save</Button>
-                    </Box>
-                </form> */}
                 <BodyForm />
             </Box>
         </Box>
